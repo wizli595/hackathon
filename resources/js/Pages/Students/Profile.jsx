bb1bfeb823cc437f0dc1ed7a77407654c5c30a34
LA-2024 <abderrezaklawni@gmail.com>
2024-04-17 14:37:15 +0100

    c3

--- resources/js/Pages/Students/Profile.jsx
+++ resources/js/Pages/Students/Profile.jsx
@@ -1,6 +1,6 @@
 import { Card } from 'primereact/card';
 import { Head } from '@inertiajs/react'
-import React from 'react'
+import React, { useState } from 'react'
 const student = {
     id: 1,
     name: "Ashish",
@@ -23,6 +23,7 @@ const student = {
     ]
 }
 function Profile() {
+    const [show, setShow] = useState(false)
     return (
         <>
             <Head title="Profile" />
@@ -41,24 +42,33 @@ function Profile() {
                         Repos : <p className='text-center text-white text-xl font-semibold ml-2'> {student.totalRepos}</p>
                     </div>
                 </div>
-                <div>
-                    <form onSubmit={handelSubmit}>
-
-                    </form>
-                </div>
-                <div className=' w-[78%] h-screen bg-orange-100 rounded-md flex gap-4 p-4'>
+                <div className=' w-[78%] h-screen bg-orange-100 rounded-md flex gap-4 p-4 relative'>
                     {student.projects.map(p => (
                         <div className="card" key={p.id}>
-                            <Card title="Simple Card">
+                            <Card title={p.name}>
                                 <p className="m-0">
-                                    {p.name}
-                                    {p.description}
-                                    {p.note}
+                                    <span className=' font-semibold'>Description :</span>  {p.description}
+                                </p>
+                                <p>
+                                    <span className=' font-semibold'>Note :</span> {p.note}
                                 </p>
                             </Card>
                         </div>
                     ))}
                 </div>
+                <button onClick={() => { setShow(true) }} className=' absolute top-8 right-14 bg-blue-500 px-4 py-1 rounded-md text-white '>Add +</button>
+                {show &&
+                    <div className='absolute h-screen w-full flex justify-center items-center'>
+                        <form class="max-w-sm mx-auto" className='w-[300px] bg-white rounded-xl shadow-md flex flex-col gap-4 p-4  relative'>
+                            <button className=' bg-red-600 text-white py-2 px-4 absolute top-[-3em] right-[-3em] rounded-full' onClick={() => { setShow(fasle) }}>X</button>
+                            <div class="mb-5">
+                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your repo name :</label>
+                                <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="repo name" />
+                            </div>
+                            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add</button>
+                        </form>
+                    </div>
+                }
             </div>
         </>
     )
